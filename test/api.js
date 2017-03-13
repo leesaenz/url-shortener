c886f9a434a65253a93c594fc1cd75a8a1d8c740
Lee Saenz <leesaenz@gmail.com>
2017-03-12 20:12:59 -0600

    Sets up test suite and first test to get all URLs.

--- test/api.js
+++ test/api.js
@@ -1,21 +1,21 @@
-var request = require('supertest');
+const expect = require('chai').expect;
+const request = require('supertest');
+const app = require('../src/server.js');
 
-describe('Routing', function(){
-
-  var server;
-
-  beforeEach(function(){
-    server = require('../src/server.js');
-  });
-
-  afterEach(function(){
-    server.close();
-  });
-
-  it('should load.', function(done) {
-    request(server)
-      .get('/api/')
+describe('CRUD Routes', () => {
+  // Test for Multiple Apps
+  it('should GET /api/v1/urls returns all urls', (done) => {
+    request(app)
+      .get('/api/v1/urls')
       .set('Accept', 'application/json')
-      .expect('Content-Type', /json/, done);
+      .expect('Content-Type', /json/)
+      .expect((res) => {
+        const urls = res.body;
+        // Save one single app from the list to test on in later tests
+        this.app = urls[0];
+
+        expect(urls.length);
+      })
+      .end(done);
   });
 });
